dddfb0f893c34324782e8de21ad0472a5d08e906
KrylovDenisK <d.krylovk@gmail.com>
2020-06-30 17:09:38 +0700

    HTTP Session

--- src/main/java/ru/job4j/dream/servlet/AuthServlet.java
+++ src/main/java/ru/job4j/dream/servlet/AuthServlet.java
@@ -1,9 +1,12 @@
 package ru.job4j.dream.servlet;
 
+import ru.job4j.dream.model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AuthServlet extends HttpServlet {
@@ -17,6 +20,9 @@ public class AuthServlet extends HttpServlet {
         String mail = req.getParameter("email");
         String password = req.getParameter("password");
         if("root@local".equals(mail) && "root".equals(password)) {
+            HttpSession sc = req.getSession();
+            User admin = new User("Admin", mail);
+            sc.setAttribute("user", admin);
             resp.sendRedirect(req.getContextPath() + "/posts.do");
         } else {
             req.setAttribute("error", "Неверный логин или пароль");
