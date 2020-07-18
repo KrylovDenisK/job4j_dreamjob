9d1d24c43719011d08a517b4a26ab8166a8a4c24
KrylovDenisK <d.krylovk@gmail.com>
2020-07-18 13:43:10 +0700

    json

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,21 +1,27 @@
 package ru.job4j.dream.servlet;
 
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
 
 public class GreetingServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println(name);
-        writer.flush();
+        String result = req.getReader().lines().collect(Collectors.joining());
+        JSONObject jsonObject = new JSONObject(result);
+        String value = jsonObject.getString("name");
+        jsonObject.put("name", "Nice to meet you " + value);
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write(jsonObject.toString());
     }
 }
