2ad161328645ff33766b1430538afa6b506681ee
KrylovDenisK <d.krylovk@gmail.com>
2020-08-02 19:09:43 +0700

    travis ci

---
+++ db/scripts/update.sql
@@ -0,0 +1,27 @@
+CREATE TABLE if not exists users (
+  id serial,
+  name character varying(100),
+  email character varying(100),
+  password character varying(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE if not exists post (
+  id serial,
+  name character varying(100),
+  PRIMARY KEY (id)
+);
+
+create table if not exists city (
+	id serial,
+	name varchar(100) not null unique,
+	primary key (id)
+);
+
+CREATE TABLE if not exists candidate (
+  id serial,
+  name character varying(100),
+  idcity integer,
+  PRIMARY KEY (id),
+  foreign key ("idCity") references city ("id")
+);
